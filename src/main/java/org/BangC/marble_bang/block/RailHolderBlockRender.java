317c9f957947a061d948d91d4d7d67c741e23806
gyxx-xc <xdll_xc@outlook.com>
2023-06-18 16:06:16 -0700

    render one segment using rail holder

--- src/main/java/org/BangC/marble_bang/block/RailHolderBlockRender.java
+++ src/main/java/org/BangC/marble_bang/block/RailHolderBlockRender.java
@@ -4,11 +4,6 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.geom.ModelPart;
-import net.minecraft.client.model.geom.PartPose;
-import net.minecraft.client.model.geom.builders.CubeDeformation;
-import net.minecraft.client.model.geom.builders.CubeListBuilder;
-import net.minecraft.client.model.geom.builders.MeshDefinition;
-import net.minecraft.client.model.geom.builders.PartDefinition;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
@@ -22,38 +17,40 @@ import org.jetbrains.annotations.NotNull;
 import org.joml.Matrix4f;
 import org.joml.Quaternionf;
 
-import java.util.SplittableRandom;
-
 public class RailHolderBlockRender implements BlockEntityRenderer<RailHolderBlockEntity> {
-    private ModelPart bb_main;
     public static final ResourceLocation LIGHT = new ResourceLocation(MarbleBang.MOD_ID, "block/light");
     public RailHolderBlockRender(BlockEntityRendererProvider.Context context) {
     }
-
     @Override
     public void render(@NotNull RailHolderBlockEntity pBlockEntity, float pPartialTick,
                        @NotNull PoseStack pPoseStack, @NotNull MultiBufferSource pBufferSource,
                        int pPackedLight, int pPackedOverlay) {
 
         pPoseStack.pushPose();
-        pPoseStack.translate(0, 0.5f, 0);
-        renderBillboardQuadBright(pPoseStack, pBufferSource.getBuffer(RenderType.solid()), 0.5f, LIGHT);
+        pPoseStack.translate(0.45, 0.45, 0.5);
+        VertexConsumer vc = pBufferSource.getBuffer(RenderType.solid());
+        Matrix4f matrix = pPoseStack.last().pose();
+        TextureAtlasSprite sprite = Minecraft.getInstance().getTextureAtlas(InventoryMenu.BLOCK_ATLAS).apply(LIGHT);
+        vc.vertex(matrix, 0.1f, 0.1f, 1).color(0xffffffff).uv(sprite.getU0(), sprite.getV0()).uv2(pPackedLight).normal(0.1f, 0, 0).endVertex();
+        vc.vertex(matrix, 0.1f, 0, 1)   .color(0xffffffff).uv(sprite.getU0(), sprite.getV0()).uv2(pPackedLight).normal(0.1f, 0, 0).endVertex();
+        vc.vertex(matrix, 0.1f, 0, 0)   .color(0xffffffff).uv(sprite.getU0(), sprite.getV0()).uv2(pPackedLight).normal(0.1f, 0, 0).endVertex();
+        vc.vertex(matrix, 0.1f, 0.1f, 0).color(0xffffffff).uv(sprite.getU0(), sprite.getV0()).uv2(pPackedLight).normal(0.1f, 0, 0).endVertex();
+
+        vc.vertex(matrix, 0, 0.1f, 0).color(0xffffffff).uv(sprite.getU0(), sprite.getV0()).uv2(pPackedLight).normal(0, 0, 0).endVertex();
+        vc.vertex(matrix, 0, 0, 0)   .color(0xffffffff).uv(sprite.getU0(), sprite.getV0()).uv2(pPackedLight).normal(0, 0, 0).endVertex();
+        vc.vertex(matrix, 0, 0, 1)   .color(0xffffffff).uv(sprite.getU0(), sprite.getV0()).uv2(pPackedLight).normal(0, 0, 0).endVertex();
+        vc.vertex(matrix, 0, 0.1f, 1).color(0xffffffff).uv(sprite.getU0(), sprite.getV0()).uv2(pPackedLight).normal(0, 0, 0).endVertex();
+
+        vc.vertex(matrix, 0.1f, 0.1f, 1).color(0xffffffff).uv(sprite.getU0(), sprite.getV0()).uv2(pPackedLight).normal(0, 1, 0).endVertex();
+        vc.vertex(matrix, 0.1f, 0.1f, 0).color(0xffffffff).uv(sprite.getU0(), sprite.getV0()).uv2(pPackedLight).normal(0, 1, 0).endVertex();
+        vc.vertex(matrix, 0, 0.1f, 0)   .color(0xffffffff).uv(sprite.getU0(), sprite.getV0()).uv2(pPackedLight).normal(0, 1, 0).endVertex();
+        vc.vertex(matrix, 0, 0.1f, 1)   .color(0xffffffff).uv(sprite.getU0(), sprite.getV0()).uv2(pPackedLight).normal(0, 1, 0).endVertex();
+
+        vc.vertex(matrix, 0.1f, 0, 0).color(0xffffffff).uv(sprite.getU0(), sprite.getV0()).uv2(pPackedLight).normal(0, -1, 0).endVertex();
+        vc.vertex(matrix, 0.1f, 0, 1).color(0xffffffff).uv(sprite.getU0(), sprite.getV0()).uv2(pPackedLight).normal(0, -1, 0).endVertex();
+        vc.vertex(matrix, 0, 0, 1)   .color(0xffffffff).uv(sprite.getU0(), sprite.getV0()).uv2(pPackedLight).normal(0, -1, 0).endVertex();
+        vc.vertex(matrix, 0, 0, 0)   .color(0xffffffff).uv(sprite.getU0(), sprite.getV0()).uv2(pPackedLight).normal(0, -1, 0).endVertex();
         pPoseStack.popPose();
     }
 
-    private static void renderBillboardQuadBright(PoseStack matrixStack, VertexConsumer builder, float scale, ResourceLocation texture) {
-        int b1 = LightTexture.FULL_BRIGHT >> 16 & 65535;
-        int b2 = LightTexture.FULL_BRIGHT & 65535;
-        TextureAtlasSprite sprite = Minecraft.getInstance().getTextureAtlas(InventoryMenu.BLOCK_ATLAS).apply(texture);
-        matrixStack.pushPose();
-        matrixStack.translate(0.5, 0.95, 0.5);
-        Quaternionf rotation = Minecraft.getInstance().gameRenderer.getMainCamera().rotation();
-        matrixStack.mulPose(rotation);
-        Matrix4f matrix = matrixStack.last().pose();
-        builder.vertex(matrix, -scale, -scale, 0.0f).color(255, 255, 255, 255).uv(sprite.getU0(), sprite.getV0()).uv2(b1, b2).normal(1, 0, 0).endVertex();
-        builder.vertex(matrix, -scale, scale, 0.0f).color(255, 255, 255, 255).uv(sprite.getU0(), sprite.getV1()).uv2(b1, b2).normal(1, 0, 0).endVertex();
-        builder.vertex(matrix, scale, scale, 0.0f).color(255, 255, 255, 255).uv(sprite.getU1(), sprite.getV1()).uv2(b1, b2).normal(1, 0, 0).endVertex();
-        builder.vertex(matrix, scale, -scale, 0.0f).color(255, 255, 255, 255).uv(sprite.getU1(), sprite.getV0()).uv2(b1, b2).normal(1, 0, 0).endVertex();
-        matrixStack.popPose();
-    }
 }
